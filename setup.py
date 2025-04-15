101b3f59fcbe5b78e6b07d160503df057927e8c7
Rushil Patel <rpatel@codegen.com>
2025-04-15 14:17:36 -0700

    fix: update setup

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ from setuptools import setup, find_packages  # noqa: H301
 #
 # prerequisite: setuptools
 # http://pypi.python.org/pypi/setuptools
-NAME = "openapi-client"
+NAME = "codegen_api_client"
 VERSION = "1.0.0"
 PYTHON_REQUIRES = ">= 3.8"
 REQUIRES = [
